94af7234d655e755c9adbb6f290ad21897a3cc89
Mike Heavers <mheavers@gmail.com>
2024-06-02 18:34:22 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = Flask(__name__)
+
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
+@app.route("/")
+def index():
+  return "Hello World!"
